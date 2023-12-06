b4619351c447024e73ccecf6c292807ab594268e
odilovdavlatbek <152914052+odilovdavlatbek@users.noreply.github.com>
2023-12-06 23:42:11 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+let a = +prompt("a sonni kiriting")
+let b = +prompt("b sonni kiriting")
+let c = a + b
+console.log(c)
+
+
+let a2 = +prompt("a sonni kiriting")
+let b2 = +prompt("b sonni kiriting")
+let c2 = a2 - b2
+console.log(c2)
+
+let a3 = +prompt("a sonni kiriting")
+let b3 = +prompt("b sonni kiriting")
+let c3 = a3 - b3
+let d = +prompt("d sonni kiriting")
+let result = c3 * d
+console.log(result)
+
+let a4 = +prompt("a sonni kiriting")
+let b4 = +prompt("b sonni kiriting")
+let c4 = a4 % b4
+console.log(c4)
+
+let a5 = +prompt("a sonni kiriting")
+let b5 = +prompt("b sonni kiriting")
+let c5 = a5 ** b5
+console.log(c5)
